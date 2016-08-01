576c76a58cfda8aa461fe43f4e7e5d0abccf6942
daimerg <daimer3007@gmail.com>
2016-08-01 12:15:07 -0500

    Manejo excepciones C#
    Manejo excepciones C#

---
+++ ExceptionEntry.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+using ReDES.Services.Common.Logging;
+using System;
+
+namespace ReDES.Services.Common.Exceptions
+{
+    public static class ExceptionEntry
+    {
+
+        /// <summary>
+        /// Método que se encarga de adicionar una entrada a las excepciones.
+        /// En caso que no exista la política de excepciones, se manejará la exepción en la política general del servicio
+        /// </summary>
+        /// <param name="ex">Descripción de la excepción</param>
+        /// <param name="policy">Política de excepciones</param>
+        /// <remarks>Hennry García. 03/19/2012</remarks>
+        public static void HandleException(string ex, string policy)
+        {
+            HandleException(new System.Exception(ex), policy);
+        }
+
+        /// <summary>
+        /// Método que se encarga de adicionar una entrada a las excepciones.
+        /// En caso que no exista la política de excepciones, se manejará la exepción en la política general del servicio
+        /// </summary>
+        /// <param name="ex">Excepción a manejar</param>
+        /// <param name="policy">Política de excepciones</param>
+        /// <remarks>Hennry García. 03/19/2012</remarks>
+        public static void HandleException(System.Exception ex, string policy)
+        {
+            Exception exceptionToThrow = null;
+            Boolean rethrow = false;
+
+            try
+            {
+                var exManager = EnterpriseLibraryContainer.Current.GetInstance<ExceptionManager>();
+                rethrow = exManager.HandleException(ex, policy, out exceptionToThrow);
+            }
+            catch
+            {
+                try
+                {
+                    var exManager = EnterpriseLibraryContainer.Current.GetInstance<ExceptionManager>();
+                    rethrow = exManager.HandleException(ex, Common.Constants.ExceptionNames.ReDESServicesPolicy, out exceptionToThrow);
+                }
+                catch (System.Exception innerEx)
+                {
+                    string errorMsg = "An unexpected exception occured while " +
+                        "calling HandleException with policy '" + policy + "'. ";
+                    errorMsg += Environment.NewLine + innerEx.ToString();
+                    errorMsg += Environment.NewLine + ex.ToString();
+                    LoggingEntry.Instance.LoggingWrite(LoggingEntry.Category.Error, LoggingEntry.Priority.Highest, errorMsg);
+                }
+            }
+            if (rethrow)
+            {
+                throw  exceptionToThrow;
+            }
+        }
+    }
+}
---
+++ GlobalExceptionHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDES.Services.Common.Exceptions
+{
+    public class GlobalExceptionHandler : IErrorHandler
+    {
+
+        public bool HandleError(Exception error)
+        {
+            ExceptionEntry.HandleException(error, ReDES.Services.Common.Constants.ExceptionNames.ReDESServicesPolicy);
+            //Representa una falla en el SOAP
+            if (error is FaultException)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void ProvideFault(Exception error, System.ServiceModel.Channels.MessageVersion version, ref System.ServiceModel.Channels.Message fault)
+        {
+            ExceptionEntry.HandleException(error, ReDES.Services.Common.Constants.ExceptionNames.ReDESServicesPolicy);
+        }
+    }
+
+    public class GlobalExceptionHandlerBehaviourAttribute : Attribute, IServiceBehavior
+    {
+        private readonly Type _errorHandlerType;
+
+        public GlobalExceptionHandlerBehaviourAttribute(Type errorHandlerType)
+        {
+            _errorHandlerType = errorHandlerType;
+        }
+
+        #region IServiceBehavior Members
+
+        public void Validate(ServiceDescription description,
+                             ServiceHostBase serviceHostBase)
+        {
+        }
+
+        public void AddBindingParameters(ServiceDescription description,
+                                         ServiceHostBase serviceHostBase,
+                                         Collection<ServiceEndpoint> endpoints,
+                                         BindingParameterCollection parameters)
+        {
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription description,
+                                          ServiceHostBase serviceHostBase)
+        {
+            IErrorHandler handler =
+                (IErrorHandler)Activator.CreateInstance(_errorHandlerType);
+
+            foreach (ChannelDispatcherBase dispatcherBase in
+                serviceHostBase.ChannelDispatchers)
+            {
+                var channelDispatcher = dispatcherBase as ChannelDispatcher;
+                if (channelDispatcher != null)
+                    channelDispatcher.ErrorHandlers.Add(handler);
+            }
+        }
+
+        #endregion
+    }
+}
